f7475c1db6aa755768f77b07b71ca74d0231440a
Aponysos <aponysos@163.com>
2017-11-23 00:26:57 +0800

    +onmouseup

--- web-mine-sweeper.js
+++ web-mine-sweeper.js
@@ -1,12 +1,43 @@
-window.addEventListener("load", drawBoard, true);
+var sz = 20, maxx = 5, maxy = 5;
+var board, ctx;
+
+window.onload = function () {
+  // initialize board element & 2d-context
+  board = document.getElementById("board");
+  ctx = board.getContext("2d");
+
+  // set board's size
+  board.width = sz * maxx;
+  board.height = sz * maxy;
+
+  // disable context menu
+  board.oncontextmenu = function (e) {
+    e.preventDefault();
+  };
+
+  // register mouse-up event
+  board.onmouseup = function (e) {
+    if (e.button == 0)
+      leftClicked(getSquarePos(board, e));
+    else if (e.button == 2)
+      rightClicked(getSquarePos(board, e));
+  };
+
+  // draw the whole board
+  drawBoard();
+};
+
+function getSquarePos(canvas, e) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: Math.floor((e.clientX - rect.left * (canvas.width / rect.width)) / sz),
+    y: Math.floor((e.clientY - rect.top * (canvas.height / rect.height)) / sz)
+  };
+}
 
 function drawBoard() {
-  var element = document.getElementById("map");
-  var ctx = element.getContext("2d");
-
   ctx.strokeStyle = "black";
   ctx.beginPath();
-  var sz = 40, maxx = 5, maxy = 5;
   for (var i = 0; i < maxy; ++i) {
     ctx.moveTo(i * sz, 0);
     ctx.lineTo(i * sz, maxy * sz);
@@ -17,4 +48,12 @@ function drawBoard() {
   }
 
   ctx.stroke();
+}
+
+function leftClicked(pos) {
+  alert("leftClicked: " + pos.x + " " + pos.y);
+}
+
+function rightClicked(pos) {
+  alert("rightClicked: " + pos.x + " " + pos.y);
 }
