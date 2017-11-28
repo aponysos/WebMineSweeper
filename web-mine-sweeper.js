f4d5d6f30813e109b8e52aed793831496151db39
aponysos <aponysos@163.com>
2017-11-28 16:43:53 +0800

    +inputs&buttons

--- web-mine-sweeper.js
+++ web-mine-sweeper.js
@@ -1,4 +1,5 @@
 var sz = 20, maxx = 5, maxy = 5;
+var inMaxx, inMaxy, btnReset;
 var elemBoard, ctx;
 var arSquares = [];
 const ST_NONE = 0;
@@ -17,13 +18,20 @@ const arMoves = [
 const bgStyle = "#FFFFFF";
 
 window.onload = function () {
+  // initialize input & button elements
+  inMaxx = document.getElementById("inMaxx");
+  inMaxy = document.getElementById("inMaxy");
+  btnReset = document.getElementById("btnReset");
+
+  // register button event
+  btnReset.onclick = function (e) {
+    //console.log("btnReset onclick: e.button = " + e.button + " e.buttons = " + e.buttons);
+    resetBoard();
+  }
+
   // initialize board element & 2d-context
   elemBoard = document.getElementById("board");
   ctx = elemBoard.getContext("2d");
-
-  // set elemBoard's size
-  elemBoard.width = sz * maxx;
-  elemBoard.height = sz * maxy;
 
   // disable context menu
   elemBoard.oncontextmenu = function (e) {
@@ -55,14 +63,7 @@ window.onload = function () {
       leftClicked(pos);
   };
 
-  // init arSquares
-  initSquares();
-
-  // draw the board
-  drawBoard();
-  for (var i = 0; i < maxx; ++i)
-    for (var j = 0; j < maxy; ++j)
-      drawSquare({ i, j });
+  resetBoard();
 };
 
 function leftClicked(pos) {
@@ -101,28 +102,49 @@ function lrClicked(pos) {
   var sq = arSquares[pos.i][pos.j];
   if (sq.revealed && sq.state > 0) {
     if (countAjacentFlagsAndMines(pos) == sq.state) {
-        for (var m in arMoves) {
-          var nextPos = { i: pos.i + arMoves[m].i, j: pos.j + arMoves[m].j };
-          if (checkBound(nextPos)) {
-            var nextSq = arSquares[nextPos.i][nextPos.j];
-            if (!nextSq.flag) {
-              nextSq.revealed = true;
-              drawSquare(nextPos);
-            }
+      for (var m in arMoves) {
+        var nextPos = { i: pos.i + arMoves[m].i, j: pos.j + arMoves[m].j };
+        if (checkBound(nextPos)) {
+          var nextSq = arSquares[nextPos.i][nextPos.j];
+          if (!nextSq.flag) {
+            nextSq.revealed = true;
+            drawSquare(nextPos);
           }
         }
+      }
     }
   }
+}
+
+function resetBoard() {
+  // read maxx & maxy from inputs
+  maxx = inMaxx.value;
+  maxy = inMaxy.value;
+
+  // set board's size
+  elemBoard.width = sz * maxx;
+  elemBoard.height = sz * maxy;
+
+  // init squares
+  initSquares();
+
+  // draw the board & all squares
+  drawBoard();
+  for (var i = 0; i < maxx; ++i)
+    for (var j = 0; j < maxy; ++j)
+      drawSquare({ i, j });
 }
 
 function drawBoard() {
   ctx.strokeStyle = "black";
   ctx.beginPath();
-  for (var i = 0; i < maxy; ++i) {
+  // vertical
+  for (var i = 0; i < maxx; ++i) {
     ctx.moveTo(i * sz, 0);
     ctx.lineTo(i * sz, maxy * sz);
   }
-  for (var i = 0; i < maxx; ++i) {
+  // horizontal
+  for (var i = 0; i < maxy; ++i) {
     ctx.moveTo(0, i * sz);
     ctx.lineTo(maxx * sz, i * sz);
   }
@@ -207,14 +229,14 @@ function createSquare(state) {
 }
 
 function countAjacentMines(pos) {
-  return countAjacent(pos, function(next){
+  return countAjacent(pos, function (next) {
     var sq = arSquares[next.i][next.j];
     return sq.state == ST_MINE;
   });
 }
 
 function countAjacentFlagsAndMines(pos) {
-  return countAjacent(pos, function(next){
+  return countAjacent(pos, function (next) {
     var sq = arSquares[next.i][next.j];
     return (sq.revealed && sq.state == ST_MINE) || (!sq.revealed && sq.flag);
   });
